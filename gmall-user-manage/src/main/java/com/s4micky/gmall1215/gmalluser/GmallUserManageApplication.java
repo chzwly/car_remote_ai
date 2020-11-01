a9055f90ced67a109760f5f226a37984c946c581
chzwly <chzwlw@gmail.com>
2020-11-01 13:03:26 +0300

    Nov-1-2020

---
+++ gmall-user-manage/src/main/java/com/s4micky/gmall1215/gmalluser/GmallUserManageApplication.java
@@ -0,0 +1,13 @@
+package com.s4micky.gmall1215.gmalluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallUserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserManageApplication.class, args);
+    }
+
+}
---
+++ gmall-user-manage/src/test/java/com/s4micky/gmall1215/gmalluser/GmallUserManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s4micky.gmall1215.gmalluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUserManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
